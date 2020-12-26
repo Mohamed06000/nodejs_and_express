1b37170c49c014949ba18fc35fd4d968cfa74258
Mohamed Mazy <mazymohamed06@gmail.com>
2020-12-26 22:55:30 +0000

    review post,put,get,delete of express. Last commit!

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const Joi = require('joi')
+
+app.use(express.json())
 
 const courses = [
     {id: 1, name: 'course1'},
@@ -17,7 +20,57 @@ app.get('/api/courses', (req,res) => {
 
 app.get('/api/courses/:id', (req,res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('Le cours introuvable')
+    if (!course) return res.status(404).send('Le cours introuvable')
+    res.send(course)
+})
+
+app.post('/api/courses', (req,res) => {
+    const { error } = validateCourse(req.body) //result.error
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+
+app.put('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) {
+        return res.status(404).send('Le cours introuvable')
+    }
+
+    const { error } = validateCourse(req.body) //result.error
+    if (error) {
+        return res.status(400).send(error)
+
+    }
+
+    course.name = req.body.name
+    res.send(course)
+})
+
+function validateCourse(course) {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.valid(course, schema)
+}
+
+
+app.delete('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('Le cours introuvable')
+
+    const index = courses.indexOf(course)
+    courses.splice(index,1)
+
     res.send(course)
 })
 
